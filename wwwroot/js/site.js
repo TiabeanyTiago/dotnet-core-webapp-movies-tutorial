4649f53b4a9f26d4e70dc4a2a5f38822d8b1f56d
Tiabeany Tiago do Espirito Santo <tiago.espiritosanto.dev@gmail.com>
2023-12-17 14:07:00 -0300

    JS to overwrite de JQuery decimal validation to use comma for the decimal values

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$.validator.methods.number = function (value, element) {
+    return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)?(?:,\d+)?$/.test(value);
+}
