b5488ac63dfb5e0a669d4dd1f939573543b3070b
Tiabeany Tiago do Espirito Santo <tiago.espiritosanto.dev@gmail.com>
2023-12-17 18:01:10 -0300

    Created validationOverwrite.js so jQuery decimals with comma are valid (Brazilian standard)

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-$.validator.methods.number = function (value, element) {
-    return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)?(?:,\d+)?$/.test(value);
-}
